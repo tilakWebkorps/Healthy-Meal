e31d114755b4f5e179fa06d1e93496909ab8499a
tilakWebkorps <tilak@webkorps.com>
2022-08-27 01:18:33 +0530

    added validations

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,4 +3,8 @@ class Recipe < ApplicationRecord
 
   has_many :meals
   has_many :meal_categories, through: :meals
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :ingredients, :presence: true
 end
